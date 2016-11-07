c1e93c64308a03925b57ab4a6be0e270f9d27fde
steffangmd1a <steffandanieldiekman@gmail.com>
2016-11-07 12:49:43 +0100

    Project afgemaakt
    Ik heb het project afgemaakt en alle extra bestanden zoals proces
    verslag toegevoegd

---
+++ CandyLandPinBall/Assets/Classes/FlipperClassRight.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperClassRight : MonoBehaviour {
+
+    // deze script is hetzelfde als FlipperClassLeft het enige verschil is dat de flipper de andere kant op draait.
+
+    public bool activateFlipper;
+    public bool deActivateFlipper;
+    public float activateTimer = 0f;
+    public float deActivateTimer = 0f;
+    public GameObject flipperPivot;
+    public static bool ismovingright = false;
+    public Quaternion originalRotation;
+    public Vector3 direction;
+    public Rigidbody ball;
+    public float force = -60;
+
+    void Start()
+    {
+        originalRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Right"))
+        {
+            activateFlipper = true;
+            ismovingright = true;
+        }
+
+        if (activateTimer > 0.06f)
+        {
+            activateFlipper = false;
+            activateTimer = 0f;
+
+        }
+
+        if (activateFlipper == true)
+        {
+            transform.RotateAround(transform.position, flipperPivot.transform.forward, -800 * Time.deltaTime);
+            activateTimer = activateTimer + Time.deltaTime;
+        }
+
+        if (Input.GetButtonUp("Right"))
+        {
+            deActivateFlipper = true;
+            ismovingright = false;
+        }
+
+        if (deActivateTimer > 0.06f)
+        {
+            deActivateFlipper = false;
+            deActivateTimer = 0f;
+
+        }
+
+        if (deActivateFlipper == true)
+        {
+            transform.rotation = originalRotation;
+            deActivateTimer = deActivateTimer + Time.deltaTime;
+        }
+    }
+}
--- CandyLandPinBall/Assets/Classes/GUIScore.cs
+++ CandyLandPinBall/Assets/Classes/GUIScore.cs
@@ -3,82 +3,123 @@ using System.Collections;
 
 public class GUIScore : MonoBehaviour {
 
+    // hier word de score mee onthouden
     public int huidigeScore;
-    public int highscore;
+    // hier worden de levens van de speler onthouden
     public int levens;
+    // deze bool bepaalt of de speler zijn eerste leven nog heeft.
     public bool bool1 = true;
+    // deze bool bepaalt of de speler zijn tweede leven nog heeft.
     public bool bool2 = true;
+    // deze bool bepaalt of de speler zijn derde leven nog heeft.
     public bool bool3 = true;
 
+    // hiermee word het plaatje die de levens aan geeft gevonden
     public Texture2D plaatje;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        // deze if word geactiveerd als je drie levens hebt
         if(levens == 3)
         {
-
+            // zet bool 1 op true.
             bool1 = true;
+            // zet bool 2 op true.
             bool2 = true;
+            // zet bool 3 op true.
             bool3 = true;
 
         }
 
-        if(levens == 2)
+        // deze if word geactiveerd als je twee levens hebt
+        if (levens == 2)
         {
 
+            // zet bool 1 op true.
             bool1 = true;
+            // zet bool 2 op true.
             bool2 = true;
+            // zet bool 3 op false.
             bool3 = false;
 
         }
 
-        if(levens == 1)
+        // deze if word geactiveerd als je één leven hebt
+        if (levens == 1)
         {
 
+            // zet bool 1 op true.
             bool1 = true;
+            // zet bool 2 op false.
             bool2 = false;
+            // zet bool 3 op false.
             bool3 = false;
+
+        }
+
+       // deze if word geactiveerd als je geen levens (minder dan 1) hebt
+        if(levens < 1)
+        {
+
+            // hiermee word je naar de gameoverscreen scene gebracht.
+            Application.LoadLevel("GameOverScreen");
 
         }
 	
 	}
 
+    // deze void word gebruit voor het maken van GUI.
     void OnGUI ()
     {
 
+        // deze if word geactiveerd als je je eerste leven hebt.
         if(bool1 == true)
         {
 
+            // hiermee word het eerste levens tekentje getekend
             GUI.DrawTexture(new Rect(0, 0, 50, 50), plaatje);
 
         }
 
-        if(bool2 == true)
+        // deze if word geactiveerd als je je derde leven hebt.
+        if (bool2 == true)
         {
 
+            // hiermee word het tweede levens tekentje getekend
             GUI.DrawTexture(new Rect(60, 0, 50, 50), plaatje);
 
         }
 
+        // deze if word geactiveerd als je je derde leven hebt.
         if (bool3 == true)
         {
 
+            // hiermee word het derde levens tekentje getekend
             GUI.DrawTexture(new Rect(120, 0, 50, 50), plaatje);
 
         }
 
+        // hiermee word de score aangegeven.
+        GUI.Box(new Rect(10, 50, 100, 25), "Score: " + huidigeScore);
+
     }
 
+    // deze void word geactiveerd via het colliderscript.
     public void Damage(int damage)
     {
 
+        // hier raak je een leven kwijt
         levens += damage;
+
+    }
+
+    // deze void word geactiveerd via het bumper script.
+    public void PlusScore(int score)
+    {
+
+        // hier krijg je 10 extra punten
+        huidigeScore += score;
 
     }
 }
