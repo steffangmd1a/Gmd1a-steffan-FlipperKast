8e774686c8397b33c6957585f5aa85b7dfc5d80c
steffangmd1a <steffandanieldiekman@gmail.com>
2016-11-21 13:23:47 +0100

    git ignore fix

---
+++ CandyLandPinBall/Assets/Classes/ColliderGameOver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderGameOver : MonoBehaviour {
+
+    // hiermee verbind ik de collider en de camera aan elkaar zodat ik een leven van de totalen levens halen.
+    public GameObject master;
+
+    // deze void word opgeroepen als de bal tegen de collider aan komt.
+    void OnTriggerEnter(Collider Player)
+    {
+        // hiermee worden de GUIScore script en de collidergameover script aan elkaar verbonden
+        GUIScore gc = (GUIScore)master.GetComponent("GUIScore");
+        // hiermee haal ik 1 punt van de levens van de speler er af.
+        gc.Damage(-1);
+
+    }
+
+}
---
+++ CandyLandPinBall/Assets/Classes/FlipperBounce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperBounce : MonoBehaviour {
+
+    // deze vector3 word gebruitk om de richting van de bal mee te bekijken.
+    public Vector3 direction;
+    // hiermee word het script met de rigidbody van de bal verbonde zodat de bal kan stuiteren.
+    public Rigidbody ball;
+    // deze float bepaalt de kracht van de bounce van de flipper.
+    public float force = -60;
+
+
+    // deze void word geactiveerd als de bal tegen de flipper aan komt.
+    public void OnCollisionEnter(Collision collision)
+    {
+        // deze if zorgt er voor dat de bal alleen weg word geschoten als je een van de twee flipper knoppen in drukt.
+        if (Input.GetButton("Left") || Input.GetButton("Right"))
+        {
+            // hier word de richting van de bal bapaald
+            direction = collision.contacts[0].point;
+
+            // hier word de bal weg geschoten door de flipper.
+            ball.AddForce(direction * force);
+        }
+
+    }
+}
